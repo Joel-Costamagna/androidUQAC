2251f084f651f7958432c353c6074cfa49af5adb
DESKTOP-9JHQE7N\babab <baptiste.boismorand1@uqac.ca>
2018-02-09 23:29:09 +0100

    debut projet (que boussole)

---
+++ app/src/main/java/com/example/babab/projetandroid/BoussoleActivity.java
@@ -0,0 +1,16 @@
+package com.example.babab.projetandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by babab on 09/02/2018.
+ */
+
+public class BoussoleActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_boussole);
+    }
+}
