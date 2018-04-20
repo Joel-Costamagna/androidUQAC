73b82b0cc68fbfd9ab0a5b57285d7cda659b9dad
NathanVoizeux <nathanvoizeux@gmail.com>
2018-04-20 17:05:59 -0400

    Est-ce que c'est bon cette fois ?

--- app/src/main/java/com/example/projet/projetandroid/meteo/MeteoActivity.java
+++ app/src/main/java/com/example/projet/projetandroid/meteo/MeteoActivity.java
@@ -1,6 +1,7 @@
 package com.example.projet.projetandroid.meteo;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
 import android.widget.TextView;
@@ -16,21 +17,27 @@ import com.example.projet.projetandroid.Helpers.MyGPSLocation;
 import com.example.projet.projetandroid.R;
 import com.google.gson.Gson;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MeteoActivity extends Activity {
     private Coord coord;
     private MyGPSLocation myGPSLocation;
-    private TextView nameTextView;
-    private TextView countryTextView;
-    private TextView coordTextView;
+    private TextView cityTextView;
+    private TextView temperatureTextView;
+    private TextView weatherIconTextView;
     private TextView descTextView;
+    private Typeface weatherFont;
     private final static String API_KEY = "0d580ba5e5cbb7b1c124a5f45a678f82";
     protected void onCreate(Bundle savedInstanceState) {
         if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
             setTheme(R.style.DarkAppTheme);
         }
         super.onCreate(savedInstanceState);
+        weatherFont = Typeface.createFromAsset(getAssets(),"fonts/weather.ttf");
         setContentView(R.layout.layout_meteo);
         myGPSLocation = new MyGPSLocation(this, new Runnable() {
             @Override
@@ -42,8 +49,8 @@ public class MeteoActivity extends Activity {
         myGPSLocation.stop();
         coord = new Coord(myGPSLocation.getLatitude(),myGPSLocation.getLongitude());
 
-        descTextView = findViewById(R.id.desc); nameTextView = findViewById(R.id.name);
-        countryTextView = findViewById(R.id.country); coordTextView = findViewById(R.id.coord);
+        descTextView = findViewById(R.id.desc); weatherIconTextView = findViewById(R.id.weather_icon);
+        cityTextView = findViewById(R.id.city); temperatureTextView = findViewById(R.id.current_temperature);
         RequestQueue queue = Volley.newRequestQueue(this);
         //TODO i18n lang code
         final String url ="http://api.openweathermap.org/data/2.5/weather?lat="+coord.getLatitude()+"&lon="+coord.getLongitude()+"&lang=fr&appid="+API_KEY;
@@ -63,12 +70,44 @@ public class MeteoActivity extends Activity {
                             descTextView.setText("Meteo : "+ base.getWeather().get(0).getDescription() + "\n"
                                     + "\nHumidité : " + base.getMain().getHumidity() + "%\n"
                                     + "\nPression : " + base.getMain().getPressure() + " hPa\n"
-                                    + "\nTempérature : " + (base.getMain().getTemp() - 273.15) + " °C\n"
                                     + "\nVitesse de vent : " + base.getWind().getSpeed() + " m/s\n"
                                     + "\nDirection du vent : " + base.getWind().getDeg() + " (" + base.getWind().getDeg() + ") \n"
                                     + "\nLever soleil : " + sunrise.get(Calendar.HOUR_OF_DAY) + ":" + sunrise.get(Calendar.MINUTE)  + ":" + sunrise.get(Calendar.SECOND) + "\n"
                                     + "\nCoucher soleil : " + sunset.get(Calendar.HOUR_OF_DAY) + ":" + sunset.get(Calendar.MINUTE)  + ":" + sunset.get(Calendar.SECOND)  + "\n");
-                        }catch (Exception error){nameTextView.setText(response + "\n" + error.getMessage());}
+                            temperatureTextView.setText( "" + (Math.round(base.getMain().getTemp() - 273.15)) + " °C\n");
+                            cityTextView.setText(""+base.getName());
+
+                            int idActuelle = base.getWeather().get(0).getId();
+                            int id = idActuelle / 100;
+                            String icon = "";
+                            if(idActuelle == 800){
+                                long currentTime = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+
+                                if(currentTime>=sunrise.get(Calendar.HOUR_OF_DAY) && currentTime<sunset.get(Calendar.HOUR_OF_DAY)) {
+                                    icon = getString(R.string.weather_sunny);
+                                } else {
+                                    icon = getString(R.string.weather_clear_night);
+                                }
+                            } else {
+                                switch(id) {
+                                    case 2 : icon = getString(R.string.weather_thunder);
+                                        break;
+                                    case 3 : icon = getString(R.string.weather_drizzle);
+                                        break;
+                                    case 7 : icon = getString(R.string.weather_foggy);
+                                        break;
+                                    case 8 : icon = getString(R.string.weather_cloudy);
+                                        break;
+                                    case 6 : icon = getString(R.string.weather_snowy);
+                                        break;
+                                    case 5 : icon = getString(R.string.weather_rainy);
+                                        break;
+                                }
+                            }
+                            weatherIconTextView.setText(icon);
+
+
+                        }catch (Exception error){cityTextView.setText(response + "\n" + error.getMessage());}
                     }
                 }, new Response.ErrorListener() {
             @Override
